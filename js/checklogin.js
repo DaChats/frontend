734b5adda3d970e7f7b0e0a9f9cdb8866ee32e17
B3ni <90302291+B3ni15@users.noreply.github.com>
2024-05-02 16:48:31 +0200

    Update index.html to include login functionality

---
+++ js/checklogin.js
@@ -0,0 +1,45 @@
+console.log('checklogin.js loaded');
+
+function addToken(token) {
+    console.log('addToken() called');
+    localStorage.setItem('token', token);
+}
+
+async function checklogin() {
+    console.log('checklogin() called');
+    const div = document.getElementById('login');
+    const token = localStorage.getItem('token');
+
+    if (token) {
+        const getUserData = await fetch(`http://localhost:3000/api/auth/login?token=${token}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+
+        if (!getUserData.ok) {
+            alert('Hiba történt a bejelentkezés során!');
+            return;
+        }
+
+        const userData = await getUserData.json();
+        console.log(userData);
+
+        const username = await userData.data.name;
+        const avatar = await userData.data.avatar;
+
+        console.log(username);
+        console.log(avatar);
+
+        div.innerHTML = `
+        <div>
+            <img style="border-radius: 360px; width: 40px; height: 40px; gap: 100px;" src="http://localhost:3000/api/files?filename=${avatar}" alt="">
+            <span style="font-size: 120%; color: white; padding: 15px;">${username}</span>
+        </div>`;
+    } else {
+        div.innerHTML = '<a class="nav-link" href="./login.html"><img src="images/login button.png" alt="Login"></a>';
+    }
+}
+
+checklogin();
