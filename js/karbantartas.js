9da4befb28400b072015727897b7bcf1daae1735
B3ni <90302291+B3ni15@users.noreply.github.com>
2024-05-16 12:50:39 +0200

    Refactor code to fix chat message display issue and optimize avatar handling

--- js/karbantartas.js
+++ js/karbantartas.js
@@ -3,8 +3,6 @@ const karbantartas = fetch('https://api.dachats.online/api/karbantartas')
     .then(response => response.json())
     .then((data) => {
         if (data.data == true) {
-            alert('Karbantartás alatt...');
-
             window.location.href = ut;
         } else {
             console.log('Nincs karbantartás alatt...');
